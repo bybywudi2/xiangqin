c5906784513f55eb018245be329505cba33bb8dd
shayminsky <1396626749@qq.com>
2019-01-14 22:26:50 +0800

    初始化聊天界面

--- pages/chat/chat.js
+++ pages/chat/chat.js
@@ -1,37 +1,119 @@
 Page({
   data: {
     openid: '1',
+    messages: [{
+        id: "msg1",
+        message: '去结婚吧',
+        messageType: 1,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg2",
+        message: '走！',
+        messageType: 0,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg3",
+        message: '真的么？？？我有点不敢相信自己的眼睛',
+        messageType: 1,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg4",
+        message: '哈哈哈，当然是假的，我一定是在做梦',
+        messageType: 0,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg5",
+        message: '是的，孩子，醒醒吧，有多少分量自己还不清楚呢',
+        messageType: 1,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg6",
+        message: ':)',
+        messageType: 0,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg7",
+        message: '滚滚滚，别来烦老娘',
+        messageType: 1,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg8",
+        message: '女神的威怒',
+        messageType: 0,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg9",
+        message: '屌丝也配有女神',
+        messageType: 1,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg10",
+        message: '你这么说就有点不对了。。。',
+        messageType: 0,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg11",
+        message: '实话实说，等你变成高富帅再说吧',
+        messageType: 1,
+        url: '../../images/photoupload.jpg'
+      },
+      {
+        id: "msg12",
+        message: '高是不可能了，变富吧，哈哈哈哈',
+        messageType: 0,
+        url: '../../images/photoupload.jpg'
+      }
+    ], // 聊天记录
+    msg: '', // 当前输入
+    scrollTop: 0, // 页面的滚动值
+    socketOpen: false, // websocket是否打开
+    tolastId: '', // 最后一条消息的ID
+    isFirstSend: true // 是否第一次发送消息(区分历史和新加)
   },
 
-  onUnload:  function(options){
+  onUnload: function (options) {
     wx.closeSocket()
+  },
+
+  onLoad(option) {
+    // 设置标题
   },
 
   onReady: function (options) {
     var that = this;
-    wx.getStorage({
-      key: 'openid',
-      success: function (res) {
-        console.log(res.data)
-        that.setData({
-          openid: res.data,
-        })
-        wx.connectSocket({
-          url: "ws://localhost:8001/websocket" + "?openid=" + that.data.openid,
-          header: {
-            'content-type': 'application/json'
-          },
-          method: "GET",
-          success() {
-            console.log('success');
-          },
-          fail() {
-            console.log('fail')
-          }
-        })
-      },
-    })
-
+    //    wx.getStorage({
+    //      key: 'openid',
+    //      success: function (res) {
+    //        console.log(res.data)
+    //        that.setData({
+    //          openid: res.data,
+    //        })
+    //        wx.connectSocket({
+    //          url: "ws://localhost:8001/websocket" + "?openid=" + that.data.openid,
+    //          header: {
+    //            'content-type': 'application/json'
+    //          },
+    //          method: "GET",
+    //          success() {
+    //            console.log('success');
+    //          },
+    //          fail() {
+    //            console.log('fail')
+    //          }
+    //        })
+    //      },
+    //    })
+    //
     //连接成功
     wx.onSocketOpen(function () {
 
@@ -48,6 +130,7 @@ Page({
     wx.onSocketError(function () {
       console.log('websocket连接失败！');
     })
+
   },
 
   formSubmit: function (e) {
@@ -56,4 +139,38 @@ Page({
     })
     console.log(e.detail.formId);
   },
+  setNickName(option) {
+    const nickname = option.nickname || '匿名聊天';
+    wx.setNavigationBarTitle({
+      title: nickname
+    });
+  },
+  // 延迟页面向顶部滑动
+  delayPageScroll() {
+    let messages = this.data.messages;
+    let length = messages.length;
+    let lastId = messages[length - 1].id;
+    this.setData({
+      tolastId: lastId
+    });
+
+  },
+  // 输入
+  onInput(event) {
+    const value = event.detail.value;
+    this.setData({
+      msg: value
+    });
+  },
+  // 聚焦
+  onFocus() {
+
+    this.setData({
+      scrollTop: 9999999
+    });
+  },
+  sendMessage() {
+    this.delayPageScroll();
+    console.log(this.data.tolastId)
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    openid:'1'
+    openid: '1'
   },
   globalData: {
     appid: 'wx782d693b3649f4fa',
@@ -17,11 +17,11 @@ Page({
     //local_getopenid_url:'http://localhost:8001/login/getOpenId',
     //test_getopenid_url:'http://39.106.194.129:8080/yulinlianaibar/login/getOpenId'
     getopenid_url: 'http://localhost:8001/login/getOpenId',
-    openid:'1',
+    openid: '1',
   },
 
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -41,21 +41,24 @@ Page({
   },
 
   onLoad: function () {
+    wx.navigateTo({
+      url: '../chat/chat'
+    })
     var that = this;
-      wx.getStorage({
-        key: 'openid',
-        success: function(res) {
-          that.setData({
-            openid: res.data,
-          })
-        },
-      })
+    wx.getStorage({
+      key: 'openid',
+      success: function (res) {
+        that.setData({
+          openid: res.data,
+        })
+      },
+    })
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -77,7 +80,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     var that = this;
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -85,40 +88,40 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-      if (true) {
-        wx.login({
-          success: res => {
-            // 发送 res.code 到后台换取 openId, sessionKey, unionId
-            if (res.code) {
-              wx.getUserInfo({
-                success: function (res_user) {
-                  wx.request({
-                    //url: getopenid_url,
-                    url: 'http://localhost:8001/login/getOpenId', 
-                    data: {
-                      code: res.code,//获取openid的话 需要向后台传递code,利用code请求api获取openid
-                      headurl: res_user.userInfo.avatarUrl,//这些是用户的基本信息
-                      nickname: res_user.userInfo.nickName,//获取昵称
-                      sex: res_user.userInfo.gender,//获取性别
-                      country: res_user.userInfo.country,//获取国家
-                      province: res_user.userInfo.province,//获取省份
-                      city: res_user.userInfo.city//获取城市
-                    },
-                    success: function (res) {
-                      console.info("succ!");
-                      wx.setStorageSync("userinfo", res.data)
-                      wx.setStorageSync("openid", res.data.userinfo.openid)//可以把openid保存起来,以便后期需求的使用
-                      that.setData({
-                        openid: res.data.userinfo.openid
-                      })
-                    }
-                  })
-                }
-              })
-            }
+    if (true) {
+      wx.login({
+        success: res => {
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          if (res.code) {
+            wx.getUserInfo({
+              success: function (res_user) {
+                wx.request({
+                  //url: getopenid_url,
+                  url: 'http://localhost:8001/login/getOpenId',
+                  data: {
+                    code: res.code, //获取openid的话 需要向后台传递code,利用code请求api获取openid
+                    headurl: res_user.userInfo.avatarUrl, //这些是用户的基本信息
+                    nickname: res_user.userInfo.nickName, //获取昵称
+                    sex: res_user.userInfo.gender, //获取性别
+                    country: res_user.userInfo.country, //获取国家
+                    province: res_user.userInfo.province, //获取省份
+                    city: res_user.userInfo.city //获取城市
+                  },
+                  success: function (res) {
+                    console.info("succ!");
+                    wx.setStorageSync("userinfo", res.data)
+                    wx.setStorageSync("openid", res.data.userinfo.openid) //可以把openid保存起来,以便后期需求的使用
+                    that.setData({
+                      openid: res.data.userinfo.openid
+                    })
+                  }
+                })
+              }
+            })
           }
-        })
-      }
+        }
+      })
+    }
 
 
   },
@@ -136,18 +139,18 @@ Page({
                   url: 'http://localhost:8001/login/getOpenId',
                   //url: app.globalData.getopenid_url,
                   data: {
-                    code: res.code,//获取openid的话 需要向后台传递code,利用code请求api获取openid
-                    headurl: res_user.userInfo.avatarUrl,//这些是用户的基本信息
-                    nickname: res_user.userInfo.nickName,//获取昵称
-                    sex: res_user.userInfo.gender,//获取性别
-                    country: res_user.userInfo.country,//获取国家
-                    province: res_user.userInfo.province,//获取省份
-                    city: res_user.userInfo.city//获取城市
+                    code: res.code, //获取openid的话 需要向后台传递code,利用code请求api获取openid
+                    headurl: res_user.userInfo.avatarUrl, //这些是用户的基本信息
+                    nickname: res_user.userInfo.nickName, //获取昵称
+                    sex: res_user.userInfo.gender, //获取性别
+                    country: res_user.userInfo.country, //获取国家
+                    province: res_user.userInfo.province, //获取省份
+                    city: res_user.userInfo.city //获取城市
                   },
                   success: function (res) {
                     console.info("succ!");
                     wx.setStorageSync("userinfo", res.data)
-                    wx.setStorageSync("openid", res.data.userinfo.openid)//可以把openid保存起来,以便后期需求的使用
+                    wx.setStorageSync("openid", res.data.userinfo.openid) //可以把openid保存起来,以便后期需求的使用
                     that.setData({
                       openid: res.data.userinfo.openid
                     })
@@ -168,5 +171,5 @@ Page({
     console.log(e.detail.formId);
   },
 
-  
+
 })
