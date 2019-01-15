aebdd1acb2e6ca601d493caccd3d1d19982adcbb
白杨 <434869482@qq.com>
2019-01-15 21:46:26 +0800

    更改配置

--- utils/url.js
+++ utils/url.js
@@ -1,6 +1,6 @@
 module.exports = {
     local: "localhost:8001",
-    test: "39.106.194.129:8080",
+    test: "39.106.194.129/yulinlianaibar",
     production: "yulinweb.xyz",
-    env: 'production'
+    env: 'test'
 }
